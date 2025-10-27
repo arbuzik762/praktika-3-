51b3446623ed91fda1ae841b039cca937c30281f
arbuzik762 <arbuzik2005maks@gmail.com>
2025-10-27 12:50:08 +0300

    Update KHB.kt

--- KHB.kt
+++ KHB.kt
@@ -1,29 +1,38 @@
 fun main() {
-    val options = listOf("1", "2", "3")
 
-    val computerChoice = options.random()
+    do{
+        val options = listOf("1", "2", "3")
 
-    println("Выберите: Камень(1), Ножницы(2), Бумага(3)")
+        val computerChoice = options.random()
     
-    val userChoice = readlnOrNull()?.trim()?.lowercase()
+        println("Выберите: Камень(1), Ножницы(2), Бумага(3)")
+    
+        val userChoice = readlnOrNull()?.trim()?.lowercase()
 
 
-    println("Вы: $userChoice")
-    println("Компьютер: $computerChoice")
-
-    when {
-        userChoice == computerChoice -> {
-            println("Ничья")
-        }
+        println("Вы: $userChoice")
+        println("Компьютер: $computerChoice")
+    
+        when {
+            userChoice == computerChoice -> {
+                println("Ничья")
+            }
         
-        (userChoice == "камень" && computerChoice == "ножницы") ||
-        (userChoice == "ножницы" && computerChoice == "бумага") ||
-        (userChoice == "бумага" && computerChoice == "камень") -> {
-            println("Победа")
+            (userChoice == "камень" && computerChoice == "ножницы") ||
+            (userChoice == "ножницы" && computerChoice == "бумага") ||
+            (userChoice == "бумага" && computerChoice == "камень") -> {
+                println("Победа")
+            }
+        
+            else -> {
+                println("Проигрыш")
+            }
+       
         }
-
-        else -> {
-            println("Проигрыш")
-        }
+        println("Снова y/n? ")
+        var x = readln()
+        if(x == "n")
+        break
     }
+    while(true)
 }
